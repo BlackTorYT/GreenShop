44af473d958d06f7a64a6e14ee96b3e0ed87dc0a
Latemaril <sukhovey.vlad@gmail.com>
2023-11-09 18:14:43 +1000

    csscomb

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@
 
 const gulp = require('gulp');
 const scss = require('gulp-sass')(require('sass'));
+const csscomb = require('gulp-csscomb');
 gulp.task('scss', function () {
     return gulp.src('resources/sass/*.scss')
         .pipe(scss().on('error', scss.logError))
         .pipe(gulp.dest('resources/css'));
+});
+
+gulp.task('csscomb', function () {
+    return gulp.src('resources/sass/**/*.scss')
+        .pipe(csscomb('yandex.json').on('error', scss.logError))
+        .pipe(gulp.dest('resources/sass'));
 });
 
 gulp.task('watch', function () {
